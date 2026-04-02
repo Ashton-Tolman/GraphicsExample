cce503359f3124896a6e2aeeb1e8d765858a1725
Ashton-Tolman <Tolman.ash22@gmail.com>
2026-04-02 08:29:27 -0600

    add buttons

--- GraphicsExample/GraphicsExampleForm.Designer.cs
+++ GraphicsExample/GraphicsExampleForm.Designer.cs
@@ -28,36 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            pictureBox1 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            DisplayPictureBox = new PictureBox();
+            DrawButton = new Button();
+            ExitButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)DisplayPictureBox).BeginInit();
             SuspendLayout();
             // 
-            // pictureBox1
+            // DisplayPictureBox
             // 
-            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            pictureBox1.BackColor = SystemColors.ActiveCaption;
-            pictureBox1.Location = new Point(12, 50);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(1567, 544);
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
+            DisplayPictureBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            DisplayPictureBox.BackColor = SystemColors.ActiveCaption;
+            DisplayPictureBox.Location = new Point(12, 50);
+            DisplayPictureBox.Name = "DisplayPictureBox";
+            DisplayPictureBox.Size = new Size(1567, 544);
+            DisplayPictureBox.TabIndex = 0;
+            DisplayPictureBox.TabStop = false;
+            // 
+            // DrawButton
+            // 
+            DrawButton.Location = new Point(1349, 662);
+            DrawButton.Name = "DrawButton";
+            DrawButton.Size = new Size(112, 34);
+            DrawButton.TabIndex = 1;
+            DrawButton.Text = "&Draw";
+            DrawButton.UseVisualStyleBackColor = true;
+            DrawButton.Click += DrawButton_Click;
+            // 
+            // ExitButton
+            // 
+            ExitButton.Location = new Point(1467, 662);
+            ExitButton.Name = "ExitButton";
+            ExitButton.Size = new Size(112, 34);
+            ExitButton.TabIndex = 2;
+            ExitButton.Text = "E&xit";
+            ExitButton.UseVisualStyleBackColor = true;
+            ExitButton.Click += ExitButton_Click;
             // 
             // GraphicsExampleForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1591, 708);
-            Controls.Add(pictureBox1);
+            Controls.Add(ExitButton);
+            Controls.Add(DrawButton);
+            Controls.Add(DisplayPictureBox);
             MinimumSize = new Size(500, 500);
             Name = "GraphicsExampleForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Graphics Example Form";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)DisplayPictureBox).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private PictureBox pictureBox1;
+        private PictureBox DisplayPictureBox;
+        private Button DrawButton;
+        private Button ExitButton;
     }
 }
