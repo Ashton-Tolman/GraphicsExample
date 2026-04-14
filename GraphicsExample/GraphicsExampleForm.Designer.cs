cb16d0837231f9c4c57793fa39db1cc319001d0f
Ashton-Tolman <Tolman.ash22@gmail.com>
2026-04-14 08:32:04 -0600

    Add context menus

--- GraphicsExample/GraphicsExampleForm.Designer.cs
+++ GraphicsExample/GraphicsExampleForm.Designer.cs
@@ -28,11 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             DisplayPictureBox = new PictureBox();
             DrawButton = new Button();
             ExitButton = new Button();
             PenColorDialog = new ColorDialog();
+            menuStrip1 = new MenuStrip();
+            FileTopMenuItem = new ToolStripMenuItem();
+            HelpTopMenuItem = new ToolStripMenuItem();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            this.DrawContextMenuItem = new ToolStripMenuItem();
+            this.ClearContextMenuItem = new ToolStripMenuItem();
+            this.ColorContextMenuItem = new ToolStripMenuItem();
+            PenSizeContextMenuItem = new ToolStripMenuItem();
+            AboutTopMenuItem = new ToolStripMenuItem();
+            DrawTopMenuItem1 = new ToolStripMenuItem();
+            ClearTopMenuItem1 = new ToolStripMenuItem();
+            ExitTopMenuItem = new ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)DisplayPictureBox).BeginInit();
+            menuStrip1.SuspendLayout();
+            contextMenuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // DisplayPictureBox
@@ -67,6 +82,85 @@
             ExitButton.UseVisualStyleBackColor = true;
             ExitButton.Click += ExitButton_Click;
             // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(24, 24);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { FileTopMenuItem, HelpTopMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1591, 33);
+            menuStrip1.TabIndex = 3;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // FileTopMenuItem
+            // 
+            FileTopMenuItem.DropDownItems.AddRange(new ToolStripItem[] { DrawTopMenuItem1, ClearTopMenuItem1, ExitTopMenuItem });
+            FileTopMenuItem.Name = "FileTopMenuItem";
+            FileTopMenuItem.Size = new Size(54, 29);
+            FileTopMenuItem.Text = "&File";
+            // 
+            // HelpTopMenuItem
+            // 
+            HelpTopMenuItem.DropDownItems.AddRange(new ToolStripItem[] { AboutTopMenuItem });
+            HelpTopMenuItem.Name = "HelpTopMenuItem";
+            HelpTopMenuItem.Size = new Size(65, 29);
+            HelpTopMenuItem.Text = "&Help";
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(24, 24);
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { this.DrawContextMenuItem, this.ClearContextMenuItem, this.ColorContextMenuItem, PenSizeContextMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(241, 165);
+            // 
+            // DrawContextMenuItem
+            // 
+            this.DrawContextMenuItem.Name = "DrawContextMenuItem";
+            this.DrawContextMenuItem.Size = new Size(240, 32);
+            this.DrawContextMenuItem.Text = "Draw";
+            // 
+            // ClearContextMenuItem
+            // 
+            this.ClearContextMenuItem.Name = "ClearContextMenuItem";
+            this.ClearContextMenuItem.Size = new Size(240, 32);
+            this.ClearContextMenuItem.Text = "Clear";
+            // 
+            // ColorContextMenuItem
+            // 
+            this.ColorContextMenuItem.Name = "ColorContextMenuItem";
+            this.ColorContextMenuItem.Size = new Size(240, 32);
+            this.ColorContextMenuItem.Text = "Color";
+            // 
+            // PenSizeContextMenuItem
+            // 
+            PenSizeContextMenuItem.Name = "PenSizeContextMenuItem";
+            PenSizeContextMenuItem.Size = new Size(240, 32);
+            PenSizeContextMenuItem.Text = "Pen Size";
+            // 
+            // AboutTopMenuItem
+            // 
+            AboutTopMenuItem.Name = "AboutTopMenuItem";
+            AboutTopMenuItem.Size = new Size(270, 34);
+            AboutTopMenuItem.Text = "&About";
+            // 
+            // DrawTopMenuItem1
+            // 
+            DrawTopMenuItem1.Name = "DrawTopMenuItem1";
+            DrawTopMenuItem1.Size = new Size(155, 34);
+            DrawTopMenuItem1.Text = "&Draw";
+            // 
+            // ClearTopMenuItem1
+            // 
+            ClearTopMenuItem1.Name = "ClearTopMenuItem1";
+            ClearTopMenuItem1.Size = new Size(155, 34);
+            ClearTopMenuItem1.Text = "&Clear";
+            // 
+            // ExitTopMenuItem
+            // 
+            ExitTopMenuItem.Name = "ExitTopMenuItem";
+            ExitTopMenuItem.Size = new Size(155, 34);
+            ExitTopMenuItem.Text = "E&xit";
+            // 
             // GraphicsExampleForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
@@ -75,12 +169,18 @@
             Controls.Add(ExitButton);
             Controls.Add(DrawButton);
             Controls.Add(DisplayPictureBox);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             MinimumSize = new Size(500, 500);
             Name = "GraphicsExampleForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Graphics Example Form";
             ((System.ComponentModel.ISupportInitialize)DisplayPictureBox).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            contextMenuStrip1.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -89,5 +189,18 @@
         private Button DrawButton;
         private Button ExitButton;
         private ColorDialog PenColorDialog;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem FileTopMenuItem;
+        private ToolStripMenuItem DrawTopMenuItem1;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem HelpTopMenuItem;
+        private ToolStripMenuItem AboutTopMenuItem;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem drawToolStripMenuItem;
+        private ToolStripMenuItem clearToolStripMenuItem;
+        private ToolStripMenuItem colorToolStripMenuItem;
+        private ToolStripMenuItem PenSizeContextMenuItem;
+        private ToolStripMenuItem ClearTopMenuItem1;
+        private ToolStripMenuItem ExitTopMenuItem;
     }
 }
